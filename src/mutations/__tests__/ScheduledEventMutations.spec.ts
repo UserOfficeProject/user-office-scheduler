bbe34f819b95b796cd9609b70d18cafc2513af8e
Peter Asztalos <peter.asztalos@sigmatechnology.se>
2020-09-28 15:09:30 +0200

    Some small tests added

---
+++ src/mutations/__tests__/ScheduledEventMutations.spec.ts
@@ -0,0 +1,28 @@
+import MockupScheduledEventDataSource from '../../datasources/mockups/ScheduledEventDataSource';
+import { ScheduledEventBookingType } from '../../models/ScheduledEvent';
+import ScheduledEventMutations from '../ScheduledEventMutations';
+
+const scheduledEventMutations = new ScheduledEventMutations(
+  new MockupScheduledEventDataSource()
+);
+
+test('test', () => {
+  const startsAt = new Date();
+  const endsAt = new Date(+startsAt + 60 * 60 * 1e3);
+
+  return expect(
+    scheduledEventMutations.create({
+      bookingType: ScheduledEventBookingType.USER_OPERATIONS,
+      description: null,
+      startsAt,
+      endsAt,
+      scheduledById: 0,
+    })
+  ).resolves.toMatchObject({
+    bookingType: ScheduledEventBookingType.USER_OPERATIONS,
+    description: null,
+    startsAt,
+    endsAt,
+    scheduledBy: { id: 0 },
+  });
+});

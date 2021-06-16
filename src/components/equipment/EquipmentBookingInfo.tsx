7d75b7997928adb6ecee891e7ae90dde1c382804
jekabskarklins <jekabs.karklins@ess.eu>
2021-06-16 20:45:19 +0300

    patch: css styling to utilize limited space in calendar better

--- src/components/equipment/EquipmentBookingInfo.tsx
+++ src/components/equipment/EquipmentBookingInfo.tsx
@@ -3,16 +3,26 @@ import React from 'react';
 
 const useStyles = makeStyles(() => ({
   container: {
-    padding: '10px 5px 5px 5px ',
+    paddingTop: '3px',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  name: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    minWidth: '100%',
+    flexGrow: 1,
   },
   title: {
-    fontSize: 15,
-    padding: '5px 0',
+    fontSize: 12,
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    padding: '1px 2px 0 0',
   },
   bookedBy: {
     fontSize: 12,
-    padding: '5px 0',
     fontStyle: 'italic',
+    padding: '1px 2px 0 0',
   },
 }));
 
@@ -30,7 +40,7 @@ function EquipmentBookingInfo({
 
   return (
     <div className={classes.container}>
-      <strong>{name}</strong>
+      <div className={classes.name}>{name}</div>
       <div className={classes.title}>{instrument}</div>
       <div className={classes.bookedBy}>{scheduledBy}</div>
     </div>

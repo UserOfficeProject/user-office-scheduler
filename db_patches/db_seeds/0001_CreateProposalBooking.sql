427ebd75e30ccfa3e899495a5d2ba6bfa4dc3479
jekabskarklins <jekabs.karklins@ess.eu>
2021-06-21 07:44:06 +0300

    changing proposal id to proposal pk

--- db_patches/db_seeds/0001_CreateProposalBooking.sql
+++ db_patches/db_seeds/0001_CreateProposalBooking.sql
@@ -3,14 +3,14 @@ $DO$
 BEGIN
 
   INSERT INTO proposal_bookings(
-    proposal_booking_id, proposal_id, call_id, created_at, updated_at, status, allocated_time, instrument_id) 
+    proposal_booking_id, proposal_pk, call_id, created_at, updated_at, status, allocated_time, instrument_id) 
     VALUES (
       1, 1, 1, now(), NOW(), 'DRAFT', 2*24*60*60, 1
     );
 
   -- add propopsal_booking that references a not existing proposal
   INSERT INTO proposal_bookings(
-    proposal_booking_id, proposal_id, call_id, created_at, updated_at, status, allocated_time, instrument_id) 
+    proposal_booking_id, proposal_pk, call_id, created_at, updated_at, status, allocated_time, instrument_id) 
     VALUES (
       2, 999, 1, now(), NOW(), 'DRAFT', 2*24*60*60, 3
     );

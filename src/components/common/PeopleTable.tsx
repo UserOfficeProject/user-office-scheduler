30f88160479c3128aaea041371b0b39646415bb0
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-08-04 12:56:58 +0200

    update yup and validation packages and fix some errors

--- src/components/common/PeopleTable.tsx
+++ src/components/common/PeopleTable.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Button from '@material-ui/core/Button';
-import makeStyles from '@material-ui/styles/makeStyles';
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import MaterialTable, { Query, Options, Column } from 'material-table';
 import React, { useState, useEffect } from 'react';
 

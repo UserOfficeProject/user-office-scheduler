2241223d14a3c68a0eb116b76db1f39639620797
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-09-28 10:46:47 +0200

    fix e2e tests

--- cypress/integration/proposal-booking.spec.ts
+++ cypress/integration/proposal-booking.spec.ts
@@ -724,12 +724,8 @@ context('Proposal booking tests ', () => {
 
       it('should be able to complete the booking process', () => {
         cy.finishedLoading();
-        cy.get('[title="Edit event"]').first().click();
-        cy.contains(/warning/i);
 
-        cy.contains(/complete the time slot booking/i).as('completeBooking');
-
-        cy.finishedLoading();
+        cy.contains(/complete proposal booking/i).as('completeBooking');
 
         cy.get('@completeBooking').should('not.be.disabled').click();
         cy.get('[data-cy="btn-ok"]').click();
@@ -737,7 +733,7 @@ context('Proposal booking tests ', () => {
         cy.wait(500);
 
         cy.contains(
-          /Time slot booking is already completed, you can not edit it/i
+          /Proposal booking is already completed, you can not edit it/i
         );
       });
 

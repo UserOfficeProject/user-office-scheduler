85e353737c31ee8c5f020ab276df965005f2dcfa
Martin Trajanovski <martin.trajanovski@ess.eu>
2021-11-30 10:15:09 +0100

    fix e2e test

--- cypress/integration/scheduled-events-timeline.spec.ts
+++ cypress/integration/scheduled-events-timeline.spec.ts
@@ -201,7 +201,7 @@ context('Scheduled events timeline tests', () => {
 
       cy.get('[data-cy="calendar-timeline-view"]').should(
         'not.contain',
-        newScheduledEvent_1.startsAt
+        newScheduledEvent_1.endsAt
       );
 
       cy.get('[data-cy="btn-view-prev"]').click();

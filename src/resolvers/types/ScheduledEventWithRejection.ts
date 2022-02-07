51d4198342fd8fa3bb4bdf82776e747ce239f90f
Martin Trajanovski <martin.trajanovski@ess.eu>
2022-02-07 10:45:10 +0100

    clean some unused dependencies and fix the resolve type of union

--- src/resolvers/types/ScheduledEventWithRejection.ts
+++ src/resolvers/types/ScheduledEventWithRejection.ts
@@ -6,4 +6,14 @@ import { ScheduledEvent } from './ScheduledEvent';
 export const ScheduledEventWithRejection = createUnionType({
   name: 'ScheduledEventWithRejection',
   types: () => [ScheduledEvent, Rejection],
+  resolveType: (value) => {
+    if ('id' in value) {
+      return ScheduledEvent;
+    }
+    if ('reason' in value) {
+      return Rejection;
+    }
+
+    return undefined;
+  },
 });

d513473fa0ad96222b6ebceb36c57ab8c5a71ace
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-03-28 07:46:10 +0200

    add service account token in the cypress.json

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -11,22 +11,3 @@
 
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
-
-import * as dotenv from 'dotenv';
-
-dotenv.config();
-
-/**
- * @type {Cypress.PluginConfig}
- */
-module.exports = (
-  on: Cypress.PluginEvents,
-  config: Cypress.PluginConfigOptions
-) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-
-  config.env.SVC_ACC_TOKEN = process.env.CYPRESS_SVC_ACC_TOKEN;
-
-  return config;
-};

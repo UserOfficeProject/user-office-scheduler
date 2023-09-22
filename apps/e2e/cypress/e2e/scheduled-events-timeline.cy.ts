42e8192da53adccea4fd565d88c0d856562b9e3b
Martin Trajanovski <martin.trajanovski@gmail.com>
2023-09-22 10:41:59 +0200

    try to improve e2e tests to prevent random fails

--- apps/e2e/cypress/e2e/scheduled-events-timeline.cy.ts
+++ apps/e2e/cypress/e2e/scheduled-events-timeline.cy.ts
@@ -45,6 +45,7 @@ context('Scheduled events timeline tests', () => {
       proposalBookingId: initialDBData.proposalBooking.id,
       bookingType: ScheduledEventBookingType.USER_OPERATIONS,
       startsAt: defaultEventBookingHourDateTime,
+      
       endsAt: getHourDateTimeAfter(24),
     };
 
@@ -180,7 +181,9 @@ context('Scheduled events timeline tests', () => {
 
       if (
         moment(getFormattedDateAfter('MMMM YYYY', 1, 'month')).month() !==
-        moment(newScheduledEvent3.startsAt).month()
+          moment(newScheduledEvent3.startsAt).month() &&
+        moment(getFormattedDateAfter('MMMM YYYY', 1, 'month')).month() !==
+          moment(newScheduledEvent3.endsAt).month()
       ) {
         cy.get('[data-cy="calendar-timeline-view"]').should(
           'not.contain',
@@ -188,6 +191,7 @@ context('Scheduled events timeline tests', () => {
         );
       } else {
         cy.contains(newScheduledEvent3.startsAt);
+        cy.contains(newScheduledEvent3.endsAt);
       }
 
       cy.get('[data-cy="calendar-timeline-view"]').should(
